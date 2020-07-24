79b9d5aa311a1f98dcdf6a2bc85bb384b745f466
Balaji Ganesan <balaji2104@gmail.com>
2020-07-23 23:32:09 -0700

    More Examples

--- src/main/java/com/balaji/fp/FP01Programming.java
+++ src/main/java/com/balaji/fp/FP01Programming.java
@@ -5,18 +5,27 @@ import java.util.List;
 public class FP01Programming {
 
 	public static void main(String[] args) {
-		printNumbersInListStructured(List.of(12,7,3,5,56,7,9,11));
 		printNumbersInListFunctional(List.of(12,7,3,5,56,7,9,11));
+		printEvenNumbersInListStructured(List.of(12,7,3,5,56,7,9,11));
 	}
-	
+
+	@SuppressWarnings("unused")
 	private static void printNumbersInListFunctional(List<Integer> numbers) {
 		numbers.stream()
 		.forEach(System.out::println); //Method Reference
 	}
 
+	@SuppressWarnings("unused")
 	private static void printNumbersInListStructured(List<Integer> numbers) {
 		for(int num : numbers) {
 			System.out.println(num);
+		}
+	}
+	
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+		for(int num : numbers) {
+			if(num%2==0)
+				System.out.println(num);
 		}
 	}
 }
